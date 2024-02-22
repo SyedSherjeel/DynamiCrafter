bb1dd0a53ecd0c952f1480c1d19eba6107308c32
SyedSherjeel <sherjeel.hashmi@vyro.ai>
2024-02-22 07:19:13 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dynamicraft',
+    version='1.1',
+    include_package_data=True,
+    package_dir={"": "src"},
+    packages=find_packages(where='src', include=['dynamicraft', 'dynamicraft.*']),
+    package_data={'': ['*.txt', '*.md', '*.gif', '*.mp4', '*.py', '*.jpg', '*.png'],},
+    # package_data={'motionctrl': ['assets/*', 'configs/*', 'examples/*', 'gradio_utils/*', 'lvdm/*', 'main/*', 'motionctrl/*', 'utils/*']},
+    author='Tencent',
+    description='A Python package for motion control and object tracking',
+    license='MIT',
+    keywords='motion control tracking',
+    url='https://github.com/SyedSherjeel/motionctrl',
+)
